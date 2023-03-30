7242a145d9c78d48b7feb5be634828c3b1e57ee1
WerWolv <werwolv98@gmail.com>
2023-03-30 17:47:52 +0200

    feat: Added pretty UI

--- include/helpers/malcore_api.hpp
+++ include/helpers/malcore_api.hpp
@@ -13,21 +13,22 @@ namespace mal::hlp {
     public:
         struct PackerInformation {
             std::string name;
-            u32 confidence;
+            u32 confidence = 0;
         };
 
         struct Signature {
             std::string title, description;
+            nlohmann::json discovered;
         };
 
         struct ThreatScore {
-            float score;
+            float score = 0.0F;
             std::vector<Signature> signatures;
         };
 
         struct Api {
             std::string apiName;
-            u64 pcValue;
+            u64 pcValue = 0x00;
 
             std::vector<std::string> arguments;
             std::string returnValue;
@@ -47,14 +48,17 @@ namespace mal::hlp {
                 auto request = createRequest("POST", "/upload");
                 auto response = request.uploadFile<std::string>(data, "filename1").get();
 
-                if (!response.isSuccess())
+                if (!response.isSuccess()) {
+                    hex::log::error("Failed to upload file to malcore: {}", response.getStatusCode());
                     return std::nullopt;
+                }
 
                 try {
                     auto json = nlohmann::json::parse(response.getData());
 
                     return json["data"]["data"]["uuid"].get<std::string>();
                 } catch (std::exception &e) {
+                    hex::log::error("Failed to get UUID: {}", e.what());
                     return std::nullopt;
                 }
             });
@@ -77,14 +81,18 @@ namespace mal::hlp {
             });
         }
 
+        static bool isAnalysisFinished(const nlohmann::json &analysis) {
+            return analysis["messages"][0]["type"].get<std::string>() == "success";
+        }
+
         static std::optional<PackerInformation> getPackerInformation(const nlohmann::json &analysis) {
             try {
-                auto &packerInformation = analysis["data"]["packer_information"];
+                auto &packerInformation = analysis["data"]["packer_information"][0];
 
                 PackerInformation result;
                 result.name = packerInformation["packer_name"].get<std::string>();
 
-                auto confidenceString = packerInformation["confidence"].get<std::string>();
+                auto confidenceString = packerInformation["percent"].get<std::string>();
                 result.confidence = std::strtoul(confidenceString.c_str(), nullptr, 10);
 
                 return result;
@@ -109,49 +117,59 @@ namespace mal::hlp {
 
         static std::optional<ThreatScore> getThreatScore(const nlohmann::json &analysis) {
             try {
-                auto &threatScore = analysis["data"]["threat_score"]["results"];
+                const auto &threatScore = analysis["data"]["threat_score"]["results"];
 
                 ThreatScore result;
                 result.score = std::strtof(threatScore["score"].get<std::string>().c_str(), nullptr);
 
                 for (auto &signature : threatScore["signatures"]) {
                     const auto &info = signature["info"];
-                    result.signatures.push_back({ info["title"].get<std::string>(), info["description"].get<std::string>() });
+                    result.signatures.push_back({
+                        info["title"].get<std::string>(),
+                        info["description"].get<std::string>(),
+                        signature["discovered"]
+                    });
                 }
 
                 return result;
-            } catch (nlohmann::json::exception &) {
+            } catch (nlohmann::json::exception &e) {
+                hex::log::error("Failed to get threat score: {}", e.what());
                 return std::nullopt;
             }
         }
 
         static std::optional<std::vector<DynamicAnalysisResult>> getDynamicAnalysisResult(const nlohmann::json &analysis) {
             try {
-                auto &dynamicAnalysis = analysis["data"]["dynamic_analysis"]["dynamic_analysis"]["entry_points"];
+                auto &dynamicAnalysisList = analysis["data"]["dynamic_analysis"]["dynamic_analysis"];
 
                 std::vector<DynamicAnalysisResult> results;
 
-                for (const auto &entryPoint : dynamicAnalysis) {
-                    DynamicAnalysisResult result;
+                for (const auto &dynamicAnalysis : dynamicAnalysisList) {
+                    for (const auto &entryPoint : dynamicAnalysis["entry_points"]) {
+                        DynamicAnalysisResult result;
 
-                    result.hash = entryPoint["apihash"].get<std::string>();
-                    for (const auto &api : entryPoint["apis"]) {
-                        Api apiResult;
-                        apiResult.apiName = api["api_name"].get<std::string>();
-                        apiResult.pcValue = std::strtoull(api["pc"].get<std::string>().c_str(), nullptr, 16);
+                        result.hash = entryPoint["apihash"].get<std::string>();
+                        for (const auto &api : entryPoint["apis"]) {
+                            Api apiResult;
+                            apiResult.apiName = api["api_name"].get<std::string>();
+                            apiResult.pcValue = std::strtoull(api["pc"].get<std::string>().c_str(), nullptr, 16);
 
-                        for (const auto &argument : api["args"])
-                            apiResult.arguments.push_back(argument.get<std::string>());
+                            for (const auto &argument : api["args"])
+                                apiResult.arguments.push_back(argument.get<std::string>());
 
-                        if (api["ret_val"].is_string())
-                            apiResult.returnValue = api["ret_val"].get<std::string>();
+                            if (api["ret_val"].is_string())
+                                apiResult.returnValue = api["ret_val"].get<std::string>();
 
-                        result.apis.emplace_back(std::move(apiResult));
+                            result.apis.emplace_back(std::move(apiResult));
+                        }
+
+                        results.emplace_back(std::move(result));
                     }
                 }
 
                 return results;
-            } catch (nlohmann::json::exception &) {
+            } catch (nlohmann::json::exception &e) {
+                hex::log::error("Failed to get dynamic analysis result: {}", e.what());
                 return std::nullopt;
             }
         }
@@ -159,6 +177,10 @@ namespace mal::hlp {
 
         static void setApiKey(std::string apiKey) {
             s_apiKey = std::move(apiKey);
+        }
+
+        static bool hasApiKey() {
+            return !s_apiKey.empty();
         }
 
         static void setUploadLimit(size_t limit) {
--- source/plugin_malcore.cpp
+++ source/plugin_malcore.cpp
@@ -6,23 +6,302 @@
 
 #include <hex/ui/view.hpp>
 
+#include <fonts/codicons_font.h>
 
-IMHEX_PLUGIN_SETUP("Malcore", "WerWolv / Internet 2.0", "Plugin to integrate with Malcore services") {
-    mal::hlp::MalcoreApi::setApiKey("00714944e8c63728edbbd54b845b4647c62819b8");
+#include <hex/api/localization.hpp>
+#include <romfs/romfs.hpp>
 
-    hex::ContentRegistry::Interface::addMenuItem({ "hex.builtin.menu.help", "Upload to Malcore" }, 10000, hex::Shortcut::None, []{
-        auto selection = hex::ImHexApi::HexEditor::getSelection();
-        auto uuid = mal::hlp::MalcoreApi::uploadProviderData(selection->getProvider(), selection->getRegion()).get();
-        if (!uuid.has_value())
-            hex::View::showErrorPopup("Failed to upload data to Malcore");
-        else {
-            auto data = mal::hlp::MalcoreApi::getAnalysisStatus(uuid.value()).get();
-            wolv::io::File file("output.json", wolv::io::File::Mode::Create);
-            file.writeString(data->dump(4));
+using namespace hex;
+
+namespace {
+
+    class ViewMalcore : public View {
+    public:
+        ViewMalcore() : View("mal.view.malcore") {
+
         }
-    }, []{
-        return hex::ImHexApi::HexEditor::isSelectionValid();
-    });
+
+        void drawContent() override {
+            if (ImGui::Begin(LangEntry(this->getUnlocalizedName()), &this->getWindowOpenState())) {
+                if (ImGui::BeginChild("##scroll", ImVec2(0, 0), false, ImGuiWindowFlags_AlwaysVerticalScrollbar)) {
+                    if (this->m_packerInformation.has_value()) {
+                        ImGui::Header("mal.view.malcore.packer"_lang, true);
+
+                        const auto &packer = this->m_packerInformation.value();
+                        ImGui::TextFormattedWrapped("mal.view.malcore.packer.text"_lang, packer.name, packer.confidence);
+
+                        ImGui::NewLine();
+                    }
+
+                    if (this->m_threatScore.has_value()) {
+                        ImGui::Header("mal.view.malcore.threat_score"_lang, true);
+
+                        const auto &threatScore = this->m_threatScore.value();
+
+                        auto color = [&threatScore]{
+                            ImColor color;
+                            color.SetHSV(3 - (threatScore.score / 100.0f) * 3, 0.8F, 0.8F);
+
+                            return color;
+                        }();
+
+                        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, u32(color));
+                        ImGui::PushItemWidth(-1);
+                        const auto text = hex::format("mal.view.malcore.threat_score.text"_lang, threatScore.score);
+                        ImGui::ProgressBar(threatScore.score / 100.0f, ImVec2(0, 0), text.c_str());
+                        ImGui::PopItemWidth();
+                        ImGui::PopStyleColor();
+
+                        ImGui::NewLine();
+
+                        int id = 1;
+                        for (const auto &[title, description, discovered] : threatScore.signatures) {
+                            ImGui::PushID(id);
+                            if (ImGui::CollapsingHeader(title.c_str())) {
+                                ImGui::TextFormattedWrapped("{}", description);
+                                ImGui::NewLine();
+                            }
+                            ImGui::PopID();
+
+                            id += 1;
+                        }
+
+                        ImGui::NewLine();
+                    }
+
+                    if (this->m_dynamicAnalysisResults.has_value()) {
+                        ImGui::Header("mal.view.malcore.dynamic_analysis"_lang, true);
+
+                        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+                        const auto &dynamicAnalysisResults = this->m_dynamicAnalysisResults.value();
+                        int id = 1;
+                        for (const auto &[hash, apis] : dynamicAnalysisResults) {
+
+                            ImGui::PushID(id);
+                            if (ImGui::CollapsingHeader(hash.c_str())) {
+                                if (ImGui::BeginTable("API", 2, ImGuiTableFlags_Borders | ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_RowBg)) {
+                                    ImGui::TableSetupColumn("mal.view.malcore.dynamic_analysis.pc"_lang);
+                                    ImGui::TableSetupColumn("mal.view.malcore.dynamic_analysis.function"_lang);
+                                    ImGui::TableHeadersRow();
+
+                                    for (const auto &api : apis) {
+                                        ImGui::TableNextRow();
+                                        ImGui::TableNextColumn();
+                                        ImGui::TextFormatted("0x{:02X}", api.pcValue);
+                                        ImGui::TableNextColumn();
+
+                                        ImGui::TextFormatted("{}(", api.apiName);
+                                        ImGui::SameLine(0, 0);
+                                        for (u32 i = 0; i < api.arguments.size(); i++) {
+                                            char *end = nullptr;
+                                            std::strtoll(api.arguments[i].c_str(), &end, 0);
+
+                                            if (end == nullptr || *end != '\0')
+                                                ImGui::TextFormattedColored(ImColor(0xFF7070E0), "\"{}\"", api.arguments[i]);
+                                            else
+                                                ImGui::TextFormattedColored(ImColor(0xFF9BC64D), "{}", api.arguments[i]);
+
+                                            ImGui::SameLine(0, 0);
+                                            if (i != api.arguments.size() - 1) {
+                                                ImGui::TextFormatted(", ");
+                                                ImGui::SameLine(0, 0);
+                                            }
+                                        }
+                                        ImGui::SameLine(0, 0);
+                                        ImGui::TextFormatted(")", api.apiName);
+                                        if (!api.returnValue.empty()) {
+                                            ImGui::SameLine(0, 0);
+                                            ImGui::TextFormatted(" -> ");
+                                            ImGui::SameLine(0, 0);
+                                            ImGui::TextFormattedColored(ImColor(0xFF9BC64D), "{}", api.returnValue);
+                                        }
+                                    }
+
+                                    ImGui::EndTable();
+                                }
+                                ImGui::NewLine();
+                            }
+                            ImGui::PopID();
+
+                            id += 1;
+                        }
+                        ImGui::PopStyleVar();
+
+                        ImGui::NewLine();
+                    }
+
+                    if (this->m_interestingStrings.has_value()) {
+                        ImGui::Header("mal.view.malcore.interesting_strings"_lang, true);
+
+                        const auto &interestingStrings = this->m_interestingStrings.value();
+                        if (ImGui::BeginTable("##interesting_strings", 1, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_ScrollY, scaled(ImVec2(0, 250)))) {
+                            ImGui::TableSetupColumn("##value");
+
+                            for (const auto &string : interestingStrings) {
+                                ImGui::TableNextRow();
+                                ImGui::TableNextColumn();
+                                ImGui::TextFormattedWrapped("{}", string);
+                            }
+
+                            ImGui::EndTable();
+                        }
+
+                        ImGui::NewLine();
+                    }
+                }
+                ImGui::EndChild();
+            }
+            ImGui::End();
+        }
+
+        void drawAlwaysVisible() override {
+            ImGui::SetNextWindowSize(scaled(ImVec2(350, 0)), ImGuiCond_Always);
+            if (ImGui::BeginPopupModal("mal.malcore.popup.api_key"_lang, nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+                if (!mal::hlp::MalcoreApi::hasApiKey()) {
+                    ImGui::TextWrapped("mal.malcore.popup.api_key.description"_lang);
+
+                    ImGui::NewLine();
+
+                    ImGui::PushItemWidth(-1);
+                    if (ImGui::InputTextIcon("##api_key", ICON_VS_SYMBOL_KEY, this->m_apiKey, ImGuiInputTextFlags_EnterReturnsTrue)) {
+                        setApiKey(this->m_apiKey);
+                        this->startAnalysisTask();
+
+                        ImGui::CloseCurrentPopup();
+                    }
+                    ImGui::PopItemWidth();
+
+                    View::confirmButtons("hex.builtin.common.yes"_lang, "hex.builtin.common.no"_lang,
+                        [this] {
+                            setApiKey(this->m_apiKey);
+                            this->startAnalysisTask();
+                            ImGui::CloseCurrentPopup();
+                        },
+                        [] {
+                            ImGui::CloseCurrentPopup();
+                        }
+                    );
+                } else {
+                    ImGui::TextWrapped("mal.malcore.popup.upload.description"_lang);
+
+                    ImGui::NewLine();
+
+                    View::confirmButtons("hex.builtin.common.okay"_lang, "hex.builtin.common.cancel"_lang,
+                        [this] {
+                            this->startAnalysisTask();
+                            ImGui::CloseCurrentPopup();
+                        },
+                        [] {
+                            ImGui::CloseCurrentPopup();
+                        }
+                    );
+                }
+
+                ImGui::EndPopup();
+            }
+        }
+
+        [[nodiscard]] bool isAvailable() const override {
+            return this->m_analysisValid && View::isAvailable();
+        }
+
+    private:
+        void startAnalysisTask() {
+            this->m_analysisValid = false;
+
+            for (const auto &highlight : this->m_highlightedAddresses)
+                ImHexApi::HexEditor::removeBackgroundHighlight(highlight);
+            for (const auto &tooltip : this->m_tooltips)
+                ImHexApi::HexEditor::removeTooltip(tooltip);
+
+            this->m_highlightedAddresses.clear();
+            this->m_tooltips.clear();
+
+            TaskManager::createTask("mal.malcore.analyzing"_lang, 0, [this](auto &) {
+                auto provider = ImHexApi::Provider::get();
+                auto uuid = mal::hlp::MalcoreApi::uploadProviderData(provider, { provider->getBaseAddress(), provider->getBaseAddress() + provider->getActualSize() }).get();
+
+                if (!uuid.has_value()) {
+                    View::showErrorPopup("mal.malcore.popup.error.upload_failed"_lang);
+                    return;
+                }
+
+                std::optional<nlohmann::json> status;
+                while (true) {
+                    status = mal::hlp::MalcoreApi::getAnalysisStatus(*uuid).get();
+
+                    if (!status.has_value()) {
+                        View::showErrorPopup("mal.malcore.popup.error.analysis_failed"_lang);
+                        return;
+                    }
+
+                    if (mal::hlp::MalcoreApi::isAnalysisFinished(*status)) {
+                        break;
+                    }
+
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                    log::info("{}", status->dump(4));
+                }
+
+                if (status.has_value()) {
+                    this->m_threatScore             = mal::hlp::MalcoreApi::getThreatScore(*status);
+                    this->m_packerInformation       = mal::hlp::MalcoreApi::getPackerInformation(*status);
+                    this->m_interestingStrings      = mal::hlp::MalcoreApi::getInterestingStrings(*status);
+                    this->m_dynamicAnalysisResults  = mal::hlp::MalcoreApi::getDynamicAnalysisResult(*status);
+
+                    this->m_analysisValid = true;
+                    this->getWindowOpenState() = true;
+                } else {
+                    View::showErrorPopup("mal.malcore.popup.error.analysis_failed"_lang);
+                }
+
+                /*TaskManager::doLater([this]{
+                    if (this->m_dynamicAnalysisResults.has_value()) {
+                        for (const auto &result : this->m_dynamicAnalysisResults.value()) {
+                            for (const auto &api : result.apis) {
+                                this->m_highlightedAddresses.push_back(ImHexApi::HexEditor::addBackgroundHighlight({ api.pcValue, 8 }, 0x80FF0000));
+                                this->m_tooltips.push_back(ImHexApi::HexEditor::addTooltip({ api.pcValue, 8 }, api.apiName, 0x80FF0000));
+                            }
+                        }
+                    }
+                });*/
+            });
+        }
+
+        static void setApiKey(const std::string &key) {
+            mal::hlp::MalcoreApi::setApiKey(key);
+            ContentRegistry::Settings::write("mal.malcore.setting.general", "hex.malcore.setting.general.api_key", key);
+        }
+
+    private:
+        std::string m_apiKey;
+
+        std::atomic<bool> m_analysisValid = false;
+        std::optional<mal::hlp::MalcoreApi::PackerInformation> m_packerInformation;
+        std::optional<std::vector<std::string>> m_interestingStrings;
+        std::optional<mal::hlp::MalcoreApi::ThreatScore> m_threatScore;
+        std::optional<std::vector<mal::hlp::MalcoreApi::DynamicAnalysisResult>> m_dynamicAnalysisResults;
+
+        std::vector<u32> m_highlightedAddresses, m_tooltips;
+    };
+
+}
+
+IMHEX_PLUGIN_SETUP("Malcore", "Internet 2.0", "Plugin to integrate with Malcore services") {
+    hex::log::debug("Using romfs: '{}'", romfs::name());
+    for (auto &path : romfs::list("lang"))
+        hex::ContentRegistry::Language::addLocalization(nlohmann::json::parse(romfs::get(path).string()));
+
+
+    auto apiKey = ContentRegistry::Settings::read("mal.malcore.setting.general", "hex.malcore.setting.general.api_key", "");
+
+    mal::hlp::MalcoreApi::setApiKey(apiKey);
+
+    ContentRegistry::Interface::addMenuItem({ "hex.builtin.menu.help", "mal.malcore.menu.help.upload_to_malcore" }, 10000, Shortcut::None, [] {
+        EventManager::post<RequestOpenPopup>("mal.malcore.popup.api_key"_lang);
+    }, ImHexApi::Provider::isValid);
+
+    ContentRegistry::Views::add<ViewMalcore>();
 }
 
 
